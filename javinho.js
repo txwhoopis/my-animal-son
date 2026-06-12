26b39cc710df47bc9700b70fa7e921aabb1e037f
someethings <giovannapinheiroalves2@gmail.com>
2026-06-12 00:52:10 -0300

    Delete javinho.js

--- javinho.js
+++
@@ -1,6 +0,0 @@
-const btnmenu = document.getElementById('btnmenu');
-const menuMobile = document.getElementById('menuMobile');
-
-btnmenu.addEventListener('click', () => {
-    menuMobile.classList.toggle('ativo');
-});
